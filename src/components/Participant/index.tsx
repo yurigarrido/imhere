663c26ef4504e9191633e8514d48c92ed3cee4a4
Yuri Garrido <y.garrido.yuri@gmail.com>
2023-08-29 11:02:04 -0300

    feat: add participant component

---
+++ src/components/Participant/index.tsx
@@ -0,0 +1,17 @@
+import { Text, TouchableOpacity, View } from "react-native";
+import { styles } from "./styles";
+
+export function Participant() {
+    return (
+        <View style={styles.container}>
+            <Text
+              style={styles.name}>
+                Yuri
+            </Text>
+
+            <TouchableOpacity style={styles.button}>
+					    <Text style={styles.buttonText}>-</Text>
+				    </TouchableOpacity>
+        </View>
+    )
+}
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,42 @@
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
+import { styles } from "./styles";
+import { Participant } from "../../components/Participant";
+
+export default function Home() {
+
+	function handleParticipantAdd() {
+		console.log('clique no botão');
+	}
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        Nome do evento
+      </Text>
+
+      <Text style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
+
+			<View style={styles.form}>
+
+				<TextInput 
+				style={styles.input}
+				placeholder="Nome do participante"
+				placeholderTextColor="#6B6B6B"
+				/>
+
+				<TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+					<Text style={styles.buttonText}>+</Text>
+				</TouchableOpacity>
+			</View>
+
+			<Participant/>
+			<Participant/>
+			<Participant/>
+			<Participant/>
+			<Participant/>
+
+    </View>
+  )
+}
